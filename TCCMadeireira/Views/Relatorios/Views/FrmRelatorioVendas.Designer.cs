320e673a43653b4f178eacf4e1fea1622b4cc410
FreqAbsoluto <Otavioalmeida650@gmail.com>
2018-06-07 18:12:31 -0300

    att

--- TCCMadeireira/Views/Relatorios/Views/FrmRelatorioVendas.Designer.cs
+++ TCCMadeireira/Views/Relatorios/Views/FrmRelatorioVendas.Designer.cs
@@ -30,36 +30,36 @@
         {
             this.components = new System.ComponentModel.Container();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.dataSetReport = new TCCMadeireira.Bancos.DataSetReport();
             this.dataTable1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSetReport = new TCCMadeireira.Bancos.DataSetReport();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
             this.dataTable1TableAdapter = new TCCMadeireira.Bancos.DataSetReportTableAdapters.DataTable1TableAdapter();
-            ((System.ComponentModel.ISupportInitialize)(this.dataSetReport)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataTable1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetReport)).BeginInit();
             this.SuspendLayout();
             // 
-            // reportViewer1
+            // dataTable1BindingSource
             // 
-            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            reportDataSource1.Name = "DataSetVendasReport";
-            reportDataSource1.Value = this.dataTable1BindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
-            this.reportViewer1.LocalReport.ReportEmbeddedResource = "TCCMadeireira.Views.Relatorios.Reports.VendasReport.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
-            this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(984, 461);
-            this.reportViewer1.TabIndex = 0;
+            this.dataTable1BindingSource.DataMember = "DataTable1";
+            this.dataTable1BindingSource.DataSource = this.dataSetReport;
             // 
             // dataSetReport
             // 
             this.dataSetReport.DataSetName = "DataSetReport";
             this.dataSetReport.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
-            // dataTable1BindingSource
+            // reportViewer
             // 
-            this.dataTable1BindingSource.DataMember = "DataTable1";
-            this.dataTable1BindingSource.DataSource = this.dataSetReport;
+            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSetVendasReport";
+            reportDataSource1.Value = this.dataTable1BindingSource;
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "TCCMadeireira.Views.Relatorios.Reports.VendasReport.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.ServerReport.BearerToken = null;
+            this.reportViewer.Size = new System.Drawing.Size(984, 461);
+            this.reportViewer.TabIndex = 0;
             // 
             // dataTable1TableAdapter
             // 
@@ -70,21 +70,21 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 461);
-            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.reportViewer);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmRelatorioVendas";
             this.Text = "Relatório de Vendas";
             this.Load += new System.EventHandler(this.FrmRelatorioVendas_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataSetReport)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataTable1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetReport)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private Bancos.DataSetMadeireiraV2TableAdapters.RELATORIO_VENDATableAdapter relatorioVendaTableAdapter;
-        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
         private Bancos.DataSetReport dataSetReport;
         private System.Windows.Forms.BindingSource dataTable1BindingSource;
         private Bancos.DataSetReportTableAdapters.DataTable1TableAdapter dataTable1TableAdapter;
