77a0eb5be91d7809103e983ebb34ae1842a20d0b
FreqAbsoluto <Otavioalmeida650@gmail.com>
2018-06-19 15:06:54 -0300

    Revert "Revert ".""
    This reverts commit 8a12b924f1a036d987fa7965f5bff60a4bfcdd5f.

--- TCCMadeireira/Models/Venda.cs
+++ TCCMadeireira/Models/Venda.cs
@@ -23,8 +23,9 @@ namespace TCCMadeireira.Models
         public DateTime DataInfo { get => dataInfo; set => dataInfo = value; }
         public decimal Valor { get => valor; set => valor = value; }
         internal Usuario Usuario { get => usuario; set => usuario = value; }
+        public string EndEntrega { get => endEntrega; set => endEntrega = value; }
 
-        public Venda(int id, Cliente cliente, Usuario usuario, List<ProdOper> produto, DateTime dataInfo, decimal valor)
+        public Venda(int id, Cliente cliente, Usuario usuario, List<ProdOper> produto, DateTime dataInfo, decimal valor, string endEntrega)
         {
             this.Id = id;
             this.Usuario = usuario;
@@ -32,23 +33,17 @@ namespace TCCMadeireira.Models
             this.Produtos = produto;
             this.DataInfo = dataInfo;
             this.Valor = valor;
-<<<<<<< HEAD
             EndEntrega = endEntrega.Trim();
-=======
->>>>>>> parent of 1497c66... .
         }
 
-        public Venda(Cliente cliente, List<ProdOper> produto, Usuario usuario, DateTime dataInfo, decimal valor)
+        public Venda(Cliente cliente, List<ProdOper> produto, Usuario usuario, DateTime dataInfo, decimal valor, string endEntrega)
         {
             this.Cliente = cliente;
             this.Usuario = usuario;
             this.Produtos = produto;
             this.DataInfo = dataInfo;
             this.Valor = valor;
-<<<<<<< HEAD
             EndEntrega = endEntrega.Trim();
-=======
->>>>>>> parent of 1497c66... .
         }
 
         public override string ToString()
--- TCCMadeireira/Views/Operacoes/FrmVenda.cs
+++ TCCMadeireira/Views/Operacoes/FrmVenda.cs
@@ -119,12 +119,8 @@ namespace TCCMadeireira.Views
                 {
                     throw new Exception("A venda não pode conter nenhum produto");
                 }
-<<<<<<< HEAD
                 Venda venda = new Venda(cliente, produtos, usuario, DateTime.Now, valor,
                     (txtEndEntrega.Text.Trim().Equals(String.Empty) ? " " : txtEndEntrega.Text));
-=======
-                Venda venda = new Venda(cliente, produtos, usuario, DateTime.Now, valor);
->>>>>>> parent of 1497c66... .
                 banco.InsertVenda(venda);
                 MessageBox.Show("Venda efetuada", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 Close();
